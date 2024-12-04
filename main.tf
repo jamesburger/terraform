91ca1201b039ea122209bae7dd8e0d6eb2696052
Jimmy Burger <burgerj723@gmail.com>
2024-12-04 16:04:38 -0500

    create new dev vpc

--- main.tf
+++ main.tf
@@ -19,12 +19,19 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Name = "HelloWorld"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
 
@@ -33,7 +40,7 @@ module "blog_sg" {
   version = "5.2.0"
   name = "blog"
 
-  vpc_id  = data.aws_vpc.default.id
+  vpc_id  = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -46,6 +53,8 @@ resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
